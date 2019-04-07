6b2af98b8bf169944fb09be1ac2898ba35e48482
Vishnu19 <vishnukp187@gmail.com>
2019-04-07 15:53:56 +0530

    ADDED Changes for PART-A Title issue

--- src/main/java/ImageHoster/repository/ImageRepository.java
+++ src/main/java/ImageHoster/repository/ImageRepository.java
@@ -50,10 +50,10 @@ public class ImageRepository {
     //Executes JPQL query to fetch the image from the database with corresponding title
     //Returns the image in case the image is found in the database
     //Returns null if no image is found in the database
-    public Image getImageByTitle(String title) {
+    public Image getImageByTitle(Integer imageId) {
         EntityManager em = emf.createEntityManager();
         try {
-            TypedQuery<Image> typedQuery = em.createQuery("SELECT i from Image i where i.title =:title", Image.class).setParameter("title", title);
+            TypedQuery<Image> typedQuery = em.createQuery("SELECT i from Image i where i.id =:imageId", Image.class).setParameter("imageId", imageId);
             return typedQuery.getSingleResult();
         } catch (NoResultException nre) {
             return null;
